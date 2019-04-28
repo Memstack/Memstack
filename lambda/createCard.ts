68cead527f85bec57c6a7a72e67a8385faf1d430
James Bourne <james.bourne@infinityworks.com>
2019-04-28 20:14:37 +0100

    Refactor responses

--- lambda/createCard.ts
+++ lambda/createCard.ts
@@ -1,63 +1,63 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import { createLogger, stdSerializers } from "bunyan";
 import "source-map-support/register";
 import { v4 as uuid } from "uuid";
+import { clientError, created, serverError } from "./response";
 
-import { createLogger, stdSerializers } from "bunyan";
-import { DynamoDB } from "aws-sdk";
-
-
-interface incomingCard {
-    front: string;
-    back: string;
+interface IncomingCard {
+  front: string;
+  back: string;
 }
 
-export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  const log = createLogger({
-    name: "createCard",
-    serializers: { err: stdSerializers.err }
-  });
-  let card : incomingCard 
-  if(event.body){
-    card = JSON.parse(event.body)
-  }
-
-  let documentClient: DynamoDB.DocumentClient;
+const getDynamoClient = (): DynamoDB.DocumentClient => {
   if (process.env.NODE_ENV === "development") {
-    documentClient = new DynamoDB.DocumentClient({
+    return new DynamoDB.DocumentClient({
       region: "localhost",
       endpoint: "http://localhost:8000",
       accessKeyId: "DEFAULT_ACCESS_KEY",
       secretAccessKey: "DEFAULT_SECRET"
     });
   } else {
-    documentClient = new DynamoDB.DocumentClient();
+    return new DynamoDB.DocumentClient();
   }
+};
+
+export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  const log = createLogger({
+    name: "createCard",
+    serializers: { err: stdSerializers.err }
+  });
+
+  if (!event.body) {
+    return clientError({ error: "No event body" });
+  }
+
+  const card: IncomingCard = JSON.parse(event.body);
+
+  const documentClient = getDynamoClient();
 
   const tableName = "Memstack";
 
   if (!tableName) {
     log.fatal("TABLE_NAME not set");
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "No table name specified" })
-    };
+    return serverError({ error: "No table name specified" });
   }
 
   const id: string = uuid();
+
   const params = {
     TableName: tableName,
     Item: {
-      "pkey": `card-${id}`,
-      "skey": "UserId:DummyUser#StackId:Stack-All",
-      "front": card!.front,
-      "back": card!.back,
+      pkey: `card-${id}`,
+      skey: "UserId:DummyUser#StackId:Stack-All",
+      front: card.front,
+      back: card.back
     }
   };
+
   await documentClient.put(params).promise();
 
-    return {
-      statusCode: 200,
-    body:'',
-    };
-  };
+  return created();
+};
