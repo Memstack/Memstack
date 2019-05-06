f4be44175061aa768f558a4f9d1c6d52c4b5a175
James Bourne <jamesbourne@outlook.com>
2019-05-06 21:43:11 +0100

    Remove schema build dir

--- schema/lib/index.js
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-export const dynamoCardSchema = yup.object({
-    pkey: yup.string().required(),
-    skey: yup.string().required(),
-    front: yup.string().required(),
-    back: yup.string().required()
-});
