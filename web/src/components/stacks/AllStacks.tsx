3385aa1e04c20b80c65aa7fd1a34026607db4918
Andy Crow <andy@memstack.io>
2019-05-12 16:28:51 +0100

    Retrieve and display stacks from api

--- web/src/components/stacks/AllStacks.tsx
+++ web/src/components/stacks/AllStacks.tsx
@@ -1,69 +1,54 @@
-import React from "react";
-import Stack from "./Stack/Stack";
-import SpanishCover from "./assets/spanish-card-image.png";
-import ItalianCover from "./assets/italian-card-background.jpg";
-import MachineLearningCover from "./assets/machine-learning-card-background.jpg";
-import PhysicsCover from "./assets/physics-card-background.jpg";
-import UXCover from "./assets/ux-card-background.png";
-import "./AllStacks.scss";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import * as Db from "../../../../schema";
 import Schedule from "../schedule/Schedule";
+import "./AllStacks.scss";
+import Stack from "./Stack/Stack";
 
-const stacks = [
-  {
-    name: "Spanish",
-    img: SpanishCover,
-    meta: "100 Cards - Proficiency: 60%",
-    description: "Useful words and phrases"
-  },
-  {
-    name: "Italian",
-    img: ItalianCover,
-    meta: "50 Cards - Proficiency: 73%",
-    description: "Basic words and phrases"
-  },
-  {
-    name: "Machine Learning",
-    img: MachineLearningCover,
-    meta: "67 Cards - Proficiency: 85%",
-    description: "SVMs, Neural Networks and Random Forests"
-  },
-  {
-    name: "Physics",
-    img: PhysicsCover,
-    meta: "1300 Cards - Proficiency: 68%",
-    description: "Revision cards for physics"
-  },
-  {
-    name: "UX",
-    img: UXCover,
-    meta: "12 Cards - Proficiency: 99%",
-    description: "User experience 101"
-  }
-];
+type Stacks = Db.Stack[];
 
-const AllStacks = () => (
-  <div className="AllStacks centered">
-    <Schedule />
-    <div className="stacks">
-      <Link to="/create-stack" className="new-stack">
-        <div className="text">
-          Add Stack
-          <i className="fas fa-plus" />
-        </div>
-      </Link>
-      {stacks.map((stack, idx) => (
-        <Stack
-          id={(idx + 1).toString()}
-          name={stack.name}
-          img={stack.img}
-          meta={stack.meta}
-          description={stack.description}
-          key={idx}
-        />
-      ))}
+const AllStacks = () => {
+  const [stacks, setStacks] = useState<Stacks>([]);
+
+  const getStacks = async () => {
+    try {
+      console.log("Getting stacks...");
+      const result = await axios.get<Stacks>(`/stacks`);
+      console.log(`result: ${JSON.stringify(result)}`);
+      setStacks(result.data && result.data["items"]);
+    } catch (err) {
+      console.error("Failed to get Stacks", err);
+    }
+  };
+
+  useEffect(() => {
+    getStacks();
+  }, []);
+
+  return (
+    <div className="AllStacks centered">
+      <Schedule />
+      <div className="stacks">
+        <Link to="/create-stack" className="new-stack">
+          <div className="text">
+            Add Stack
+            <i className="fas fa-plus" />
+          </div>
+        </Link>
+        {stacks.map((stack, idx) => (
+          <Stack
+            id={(idx + 1).toString()}
+            name={stack.title}
+            img={stack.image}
+            meta={""}
+            description={stack.description}
+            key={idx}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AllStacks;
