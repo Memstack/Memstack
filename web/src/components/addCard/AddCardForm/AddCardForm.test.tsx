d3a1885c12bba32341f2af89e2b0e8717d6d6094
Andy Crow <andy@memstack.io>
2019-05-12 16:47:43 +0100

    Refactor AddCard and AddCardForm into single component

--- web/src/components/addCard/AddCardForm/AddCardForm.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AddCardForm from "./AddCardForm";
-import { shallow } from "enzyme";
-
-describe("AddCardForm", () => {
-  it("renders without crashing", () => {
-    shallow(<AddCardForm />);
-  });
-});
