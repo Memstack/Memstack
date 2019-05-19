fca328684edb9156f2efb1b52a17be8c48f189b9
Andy Crow <andy@memstack.io>
2019-05-19 20:05:59 +0100

    Remove unused library + minor refactorings

--- serverless/lambda/uploadFile.it.test.ts
+++ serverless/lambda/uploadFile.it.test.ts
@@ -1,14 +1,11 @@
 import { agent } from "supertest";
 import { getEndpoint } from "./tests/setup";
 import jwt from "jsonwebtoken";
-import urlParse from "url-parse";
 
 describe("POST /uploadFile", () => {
   it("it responds 200 with an s3 presigned url", async () => {
     const filename = "cover-image.png";
-
     const userId = "1234";
-
     const token = jwt.sign({ sub: userId }, "super-secret");
 
     const res = await agent(getEndpoint())
@@ -17,8 +14,8 @@ describe("POST /uploadFile", () => {
       .set("Authorization", `Bearer ${token}`)
       .expect(200);
 
-    const url = urlParse(res.body.url);
-    const pathParts = url.pathname.split("/");
+    const presignedUrl = new URL(res.body.url);
+    const pathParts = presignedUrl.pathname.split("/");
 
     expect(pathParts[1]).toEqual(userId);
     expect(pathParts[2].endsWith(`_${filename}`)).toBeTruthy();
