d6d75eec04a2ea503cc3c55f638b2caaed90391c
Andy Crow <andy@memstack.io>
2019-05-19 14:10:37 +0100

    Set button onClick to submitForm in AddStack

--- web/src/components/stacks/AddStack/AddStack.tsx
+++ web/src/components/stacks/AddStack/AddStack.tsx
@@ -10,78 +10,78 @@ import FileInput from "../../form/files/FileInput";
 type AddStackFormValues = Db.IncomingStack & Db.GeneratedStack;
 
 const AddStack = () => (
-    <div className="add-stack">
-      <div className="add-stack-form">
-        <Formik
-          initialValues={{ title: "", image: "", description: "" }}
-          validate={(values: AddStackFormValues) => {
-            let errors: any = {};
-            if (!values.title) {
-              errors.title = "Required";
-            }
-            if (!values.description) {
-              errors.description = "Required";
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setSubmitting(false);
-          }}
-        >
-          {({ values, handleSubmit }) => {
-            return (
-              <>
-                <div className="title">
-                  <h2>Add Stack</h2>
-                  <Divider />
-                </div>
-                <Field
-                  name="image"
-                  render={({ field, form }) => (
-                    <FileInput
-                      className="image-upload"
-                      form={form}
-                      field={field}
-                      name="image"
-                      accept="image/*"
-                      placeholder="Upload Cover Image"
-                      icon="fas fa-image"
-                    />
-                  )}
-                />
-                <Field
-                  name="title"
-                  render={({ field, form }) => (
-                    <TextInput
-                      field={field}
-                      form={form}
-                      extra={{
-                        placeholder: "Stack Name",
-                        fluid: true
-                      }}
-                    />
-                  )}
-                />
-                <Field
-                  name="description"
-                  render={({ field, form }) => (
-                    <TextInput
-                      field={field}
-                      form={form}
-                      extra={{
-                        placeholder: "Description",
-                        fluid: true
-                      }}
-                    />
-                  )}
-                />
-                <Button text="Save" />
-              </>
-            );
-          }}
-        </Formik>
-      </div>
+  <div className="add-stack">
+    <div className="add-stack-form">
+      <Formik
+        initialValues={{ title: "", image: "", description: "" }}
+        validate={(values: AddStackFormValues) => {
+          let errors: any = {};
+          if (!values.title) {
+            errors.title = "Required";
+          }
+          if (!values.description) {
+            errors.description = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(false);
+        }}
+      >
+        {({ values, submitForm }) => {
+          return (
+            <>
+              <div className="title">
+                <h2>Add Stack</h2>
+                <Divider />
+              </div>
+              <Field
+                name="image"
+                render={({ field, form }) => (
+                  <FileInput
+                    className="image-upload"
+                    form={form}
+                    field={field}
+                    name="image"
+                    accept="image/*"
+                    placeholder="Upload Cover Image"
+                    icon="fas fa-image"
+                  />
+                )}
+              />
+              <Field
+                name="title"
+                render={({ field, form }) => (
+                  <TextInput
+                    field={field}
+                    form={form}
+                    extra={{
+                      placeholder: "Stack Name",
+                      fluid: true
+                    }}
+                  />
+                )}
+              />
+              <Field
+                name="description"
+                render={({ field, form }) => (
+                  <TextInput
+                    field={field}
+                    form={form}
+                    extra={{
+                      placeholder: "Description",
+                      fluid: true
+                    }}
+                  />
+                )}
+              />
+              <Button text="Add" onClick={submitForm} />
+            </>
+          );
+        }}
+      </Formik>
     </div>
+  </div>
 );
 
 export default AddStack;
